66b404ceb52f4a3510b54faaca58a18fa90e6ff3
Yue Zhou <26257959+mrwrongwrongwrong@users.noreply.github.com>
2021-11-30 00:10:04 -0500

    Add files via upload

---
+++ FinalProject/nov30-veersion00/yue/Prettyprint.ml
@@ -0,0 +1,61 @@
+open Abstractsyntax;;
+open Str;;
+open Stack;;
+open Printf;;
+
+(** 
+open Parser.mly;;
+**)
+
+
+(*Location is e.e *)
+(*In expression, there are different types of expression: 1.field expression 2.Arithm expression 3. location expr 4. recursive procedure expr 5.Boolean expr *)
+
+(*prog is the tree structure here *)
+let ( string_of_command, string_of_bool_expression, string_of_expression)= 
+  let rec string_of_command = function
+    | VarDecl (v, c) -> Printf.sprintf("Var_Declaration(%s") v ^ "," ^ string_of_command c ^ ")"
+    | Call (e1, e2) -> Printf.sprintf("Call:(") ^ string_of_expression e1 ^ "," ^ string_of_expression e2 ^ ")"
+    | Malloc v -> Printf.sprintf("Malloc: (%s") v ^ ")"
+    | Assign (v, e) -> Printf.sprintf("Assign:(%s") v ^ string_of_expression e ^ ")" 
+    | FieldAssign (e1, e2, e3) -> Printf.sprintf("FieldAssign:(") ^ string_of_expression e1 ^ "," ^ string_of_expression e2 ^ "," ^ string_of_expression e3 ^ ")"
+    | Sequence (c1, c2) -> Printf.sprintf("Sequence:(") ^ string_of_command c1 ^ "," ^ string_of_command c2 ^ ")"
+    | While (b, c) -> Printf.sprintf("While:(") ^ string_of_bool_expression b ^ ", loop_body:" ^ string_of_command c ^ ")"
+    | If (b, c1, c2) -> Printf.sprintf("If:(") ^ string_of_bool_expression b ^ ", then" ^ string_of_command c1 ^ ", else" ^ string_of_command c2 ^ ")"
+    | Parallel (c1, c2) -> Printf.sprintf("Parallel:(") ^ (string_of_command c1) ^ "|||" ^ string_of_command c2 ^ ")"
+    | Atom c -> Printf.sprintf("Atom:(") ^ string_of_command c ^ ")"
+    | Skip -> Printf.sprintf("SKIP")
+  and 
+  string_of_expression en = match en with
+    | Field st -> Printf.sprintf("Field:%s") st 
+    | One n -> Printf.sprintf("One:%d") n
+    | Minus (e1, e2) -> Printf.sprintf("Minus:(") ^ string_of_expression e1 ^ "," ^ string_of_expression e2 ^ ")"
+    | Null -> Printf.sprintf("Null")
+    | Dot (e1, e2) -> Printf.sprintf("Location:(") ^ string_of_expression e1 ^ "," ^ string_of_expression e2 ^ ")"
+    | Procedure (v, c) -> Printf.sprintf("Proc:(") ^ v ^ "," ^ string_of_command c ^ ")"
+    | Var v1 -> Printf.sprintf("Var:(") ^ v1 ^ ")"
+  (*type expression = Field of field | One of int | Minus of expression * expression | Null | Var of var | Dot of expression * expression | Procedure of var * cmds *)
+  and 
+  string_of_bool_expression (bool_expr:bool_expression) = match bool_expr with
+    | Expression (e1, bop, e2) -> (match bop with 
+      | EQ -> "(" ^ string_of_expression e1 ^ "==" ^ string_of_expression e2 ^ ")"
+      | LT -> "(" ^ string_of_expression e1 ^ "<" ^ string_of_expression e2 ^ ")"
+    )
+    | Bool bo -> Printf.sprintf("%b") bo
+    (**| true -> Printf.sprintf("true")
+    | false -> Printf.sprintf("false") **)
+  in (string_of_command, string_of_bool_expression, string_of_expression)
+
+(**    | _ -> Printf.sprintf("Unmatched Print Case in string_of_command")
+       | _ -> Printf.sprintf("Unmatched Print Case in string_of_expression")
+       | _ -> Printf.sprintf("Unmatched Print Case in string_of_bool_expression")
+**)
+(** 
+let rec string_list string_function lst = 
+  "{" ^ (string_list2 string_function lst)
+
+and 
+string_list2 string_function lst = match lst with
+  | hd::tl -> (string_function hd) ^ (",\n") ^ (string_list2 string_function tl)
+  | [] -> "}"
+**)
---
+++ FinalProject/nov30-veersion00/yue/main.ml
@@ -0,0 +1,22 @@
+open Abstractsyntax;;
+open Staticsemantics;;
+open Transitionalsemantics;;
+open Prettyprint;;
+(**open Parser;;
+open Lexer;;
+**)
+
+let ic = open_in Sys.argv.(1) in 
+    let lexbuffer = (Lexing.from_channel ic) in
+        let my_abstract_syntax_tree = Parser.cmd Lexer.token lexbuffer in (
+            print_string "Abstract Syntax tree:\n";
+            let my_print_tree = string_of_command my_abstract_syntax_tree in
+            print_string (my_print_tree);
+            print_string "\n\n";
+            print_string "The Output of My Program: \n";
+            (* let final_state = transitional_semantics my_abstract_syntax_tree in *)
+            print_string ("\n Final State: \n" ^ (string_of_state (transitional_semantics my_abstract_syntax_tree)));
+            print_newline()
+        )
+
+
